d216939aa85816228359f5ccd6250d8b34e06dbd
Anjlesh Wasule <154040674+Anjlesh-Wasule@users.noreply.github.com>
2026-02-18 14:48:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your code here
+let draggedDiv = null;
+
+const images = document.querySelectorAll(".image");
+
+images.forEach(div => {
+
+  div.addEventListener("dragstart", () => {
+    draggedDiv = div;
+    div.classList.add("selected");
+  });
+
+  div.addEventListener("dragend", () => {
+    draggedDiv = null;
+    div.classList.remove("selected");
+  });
+
+  div.addEventListener("dragover", (e) => {
+    e.preventDefault(); // REQUIRED
+  });
+
+  div.addEventListener("drop", () => {
+    if (draggedDiv === div) return;
+
+    // swap background images
+    const temp = draggedDiv.style.backgroundImage;
+    draggedDiv.style.backgroundImage = div.style.backgroundImage;
+    div.style.backgroundImage = temp;
+  });
+
+});
